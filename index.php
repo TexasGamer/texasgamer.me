a36794077a89b8f7ddb77cbb8a77e3498257c67b
TexasGamer <tpgaubert@gmail.com>
2014-09-12 15:11:55 -0500

    Initial commit of website

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="res/img/favicon.ico">
+
+		<title>Thomas Gaubert</title>
+
+		<link href="res/css/bootstrap.min.css" rel="stylesheet">
+		<link href="res/css/font-awesome.min.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+		<link href="res/css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="bg1"></div>
+		<!-- 
+		<div id="name">
+			Thomas Gaubert
+			<p>University of Texas &middot; Computer Science</p>
+		</div>
+		-->
+
+		<!-- About -->
+		<div id="about">
+			<a href="http://google.com/+ThomasGaubert"><img class="img-circle" src="res/img/profile.jpg"></img></a>
+			<p>Thomas Gaubert</p>
+		</div>
+		
+		<!-- Projects -->
+		<div id="projects">
+			<div class="btn-group project-btn">
+				<a class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+					Projects <span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu dropdown-menu-right" role="menu">
+					<li class="dropdown-header">Android</li>
+					<li><a href="projects/tockle"><img src="res/img/tockle.png" width="20" height="20" style="vertical-align: middle;"></img> Tockle</a></li>
+					<li><a href="projects/skcraft-mobile"><img src="res/img/skcraft-mobile.png" width="20" height="20" style="vertical-align: middle;"></img> SKCraft Mobile</a></li>
+					<li class="divider"></li>
+					<li><a href="http://github.com/TexasGamer">More on GitHub</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<!-- Featured -->
+		<!-- 
+		<div id="featured">
+			<a href="http://texasgamer.me/projects/tockle"><img src="res/img/tockle.png"></img></a>
+			<p>Tockle</p>
+			<small>Control your phone from Android Wear</p>
+		</div>
+		-->
+		
+		<!-- Links -->
+		<div id="links">
+			<a class="github" href="http://github.com/TexasGamer" target="_blank"><i class="fa fa-github-square"></i> GitHub &middot;</a>
+			<a class ="resume" href="resume.pdf" target="_blank"><i class="fa fa-file-text-o"></i> Resume &middot;</a>
+			<a class ="gplus" href="http://www.google.com/+ThomasGaubert" target="blank"><i class="fa fa-google-plus-square"></i> Google+ &middot;</a>
+			<a class ="twitter" href="http://www.twitter.com/ThomasGaubert" target="blank"><i class="fa fa-twitter-square"></i> Twitter &middot;</a>
+			<a class ="email" href="mailto:tpgaubert@gmail.com" target="_blank"><i class="fa fa-envelope"></i> Email</a>
+		</div>
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="res/js/bootstrap.min.js"></script>
+	</body>
+</html>
